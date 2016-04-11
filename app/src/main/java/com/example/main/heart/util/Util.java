d28b22e3534c70450e5c7a6369cac313b6ebfd6f
weitianming <qq806783737@live.com>
2016-04-11 21:53:26 +0800

    Initial commit

---
+++ app/src/main/java/com/example/main/heart/util/Util.java
@@ -0,0 +1,11 @@
+package com.example.main.heart.util;
+
+public class Util {
+
+	public static int centerStartingX, centerStartingY; // ������
+	public static int centerEndX, centerEndY; // ����յ�
+	public static double ScaleX, ScaleY; // �̶ȼ��
+	public static float core; // Y������
+	public static double spacingY;
+
+}
